fa27a6f46b163fdad3aa2dbcfee1f0ea8967ed98
yumeangelica <74900808+yumeangelica@users.noreply.github.com>
2024-08-30 18:00:50 +0300

    update parameter

--- src/get_uv_index.py
+++ src/get_uv_index.py
@@ -1,6 +1,6 @@
 import requests
 
-def get_uv_index(api_key: str, lat: float, lon: float):
-    url = f'http://api.openweathermap.org/data/2.5/uvi?appid={api_key}&lat={lat}&lon={lon}'
+def get_uv_index(owm_api_key: str, lat: float, lon: float):
+    url = f'http://api.openweathermap.org/data/2.5/uvi?appid={owm_api_key}&lat={lat}&lon={lon}'
     response = requests.get(url).json()
-    return response['value'] # Returns the uv index value from the response
+    return response['value']  # Returns the UV index value from the response
